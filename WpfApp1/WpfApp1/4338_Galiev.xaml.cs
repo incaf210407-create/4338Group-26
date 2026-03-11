97888df546f993f7f6a64800f0b2d7ea74eef171
insaf <insafgaliev@gmail.com>
2026-03-11 23:04:58 +0300

    Added author info button and window for 4338_Galiev

---
+++ WpfApp1/WpfApp1/4338_Galiev.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class _4338_Galiev : Window
+    {
+        public _4338_Galiev()
+        {
+            InitializeComponent();
+        }
+    }
+}
